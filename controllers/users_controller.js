145cf9103e46ff1276259c6ab7e16f425a85e09e
Aditya Nayak <nayak.aditya998@example.com>
2021-07-20 10:45:51 +0530

    User SignIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: "User Home"
-    });
+   if(req.cookies.user_id){
+       User.findById(req.cookies.user_id, function(err, user){
+           if(user){
+            return res.render('user_profile', {
+                title: "User Home",
+                user: user
+            })
+           }else{
+               return res.redirect('/users/sign-in');
+           }
+       });
+   }else{
+       return res.redirect('/users/sign-in');
+   }
 }
 
 //Render the Sign up page
@@ -46,5 +57,33 @@ module.exports.create = function(req, res){
 
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //TODO later
+    
+    // steps to authenticate
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        //Handle user found
+
+        if(user){
+             //Handle password which doesn't match
+             if(user.password != req.body.password){
+                 return res.redirect('back');
+             }
+
+            //Handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+             //Handle user not found
+
+             return res.redirect('back');
+
+        }
+    });
+
+
+    
+ 
 }
