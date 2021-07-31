b5bf216a8c886dbe7ef2a569f97424bf0037c726
Aditya Nayak <nayak.aditya998@example.com>
2021-07-31 16:44:40 +0530

    Deleting posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,9 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-const postController = require('../controllers/posts_controller');
+const postsController = require('../controllers/posts_controller');
 
-router.post('/create', passport.checkAuthentication, postController.create);
+router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 
 module.exports = router;
