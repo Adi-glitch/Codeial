f42d7288446e83c8d2392e554621da52e5e091fa
Aditya Nayak <nayak.aditya998@example.com>
2021-08-11 09:43:48 +0530

    Setting Up directory structure for APIs and Postman

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const postsApi = require('../../../controllers/api/v1/posts_api');
 
 
-router.use('/v1', require('./v1'));
+router.get('/', postsApi.index);
 
 
 
